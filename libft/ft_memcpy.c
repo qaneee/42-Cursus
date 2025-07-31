50ba67e2df30a7c2f0ee5dd12381be47a9ceca7b
qaneee <vardanyanarev48@gmail.com>
2025-07-31 12:27:40 +0400

    libft

---
+++ libft/ft_memcpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arvardan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/23 15:03:33 by arvardan          #+#    #+#             */
+/*   Updated: 2025/02/01 14:13:51 by arvardan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	size_t			i;
+	unsigned char	*ptr1;
+	unsigned char	*ptr2;
+
+	i = 0;
+	ptr1 = (unsigned char *)dst;
+	ptr2 = (unsigned char *)src;
+	if (n < 0 || (!src && !dst))
+		return (NULL);
+	while (i < n)
+	{
+		*(ptr1 + i) = *(ptr2 + i);
+		i++;
+	}
+	return (dst);
+}
